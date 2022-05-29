176c2ab491613c96aad4de49ec38a144d48872d5
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-05-29 20:30:59 +0700

    Add files via upload

--- plugins/play.js
+++ plugins/play.js
@@ -1,93 +1,33 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let fs = require('fs')
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let yt2 = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+const YT = require('../../../lib/yt')
+    let handler = async (m, conn, { prefix, text, cmd }) => {
+        try {
+            if (args.length < 1) return m.reply(`*Fitur mencari Vidio full tag metadata, sangat disarankan unutk memasukkan judul yang tepat*\n${prefix}${cmd} judul - artis\n\ncontoh : ${prefix}${cmd} Garox viral `)
+            const arr = await YT.search(args.join(' '))
+            let list = new Array();
+            let desc = ` *Play Youtube* \n𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙧 𝙙𝙚𝙣𝙜𝙖𝙣 𝙛𝙪𝙡𝙡 𝙩𝙖𝙜 𝙢𝙚𝙩𝙖𝙙𝙖𝙩𝙖\n\n𝘿𝙞𝙩𝙚𝙢𝙪𝙠𝙖𝙣  *${arr.length}* *Vidio* `
+            for (let i = 0; i < arr.length; i++) {
+                list.push({
+                    title: `${i + 1}. ${arr[i].title}`,
+                    description: `Channel : ${arr[i].artist}\nAlbum : ${arr[i].album}\nDuration : ${arr[i].duration.label}\nSource : ${arr[i].isYtMusic ? 'YouTube Vidio' : 'YouTube'}\nId : ${arr[i].id}`,
+                    rowId: `${prefix}ytmp4 ${arr[i].url}`
+                });
+            }
+            await conn.sendListM(
+                m.chat,
+                { buttonText: 'Video Downloader', description: desc, title: 'Pilih untuk mendownload' },
+                list,
+                m
+            )
+
+        } catch (error) {
+            m.reply(util.format(error))
+            console.log(error);
+        }
     }
-  }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-let anu =  `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-*link sumber:* 
-${vid.url}
-
-`
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: anu,
-           locationMessage: { 
-           jpegThumbnail: await (await fetch(thumb)).buffer() }, 
-           hydratedFooterText: `jika video tidak sesuai
-silahkan ketik .yta link YouTube untuk mengunduh audio
-dan ketik .ytv link YouTube Untuk Mengunduh Video`,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '📍instagram',
-               url: instagram
-             }
-
-           },
-               {
-             quickReplyButton: {
-               displayText: 'video 360p',
-               id: `.ytmp4 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'video 720p',
-               id: `.ytv720 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'Audio',
-               id: `.ytmp3 ${vid.url}`,
-             }
-
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = /^(p|play)$/i
-
-handler.exp = 0
+handler.help = ['play'].map(v => v + ' <query>')
+handler.tags = ['internet']
+handler.command = /^(playstore)$/i
+handler.limit = true
 
 module.exports = handler
-
